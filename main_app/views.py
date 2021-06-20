c286c54db767b5e73a42f0ce3571fdb7992d51c6
dcidcoronado <daniel.cid.coronado@gmail.com>
2021-06-20 18:49:38 -0400

    entrega?

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 import random
+from time import gmtime, strftime
 
 def index(request):
     if 'gold' not in request.session:
@@ -11,22 +12,39 @@ def process_money(request):
     if 'activities' not in request.session:
         request.session['activities'] = []
     earns = request.POST['earns']
-    farm = random.randint(10, 20)
-    cave = random.randint(5, 10)
-    house = random.randint(2, 5)
-    casino = random.randint(-50, 50)
+    date = strftime("%Y/%m/%d", gmtime())
+    time = strftime("%I:%M %p", gmtime())
+    clase = 1
+    gold = 0
     if earns == 'farm':
-        request.session['gold'] += farm
-        request.session['activities'].insert(0, f'Earned {farm} golds from the farm!')
-    if earns == 'cave':
-        request.session['gold'] += cave
-        request.session['activities'].insert(0, f'Earned {cave} golds from the cave!')
-    if earns == 'house':
-        request.session['gold'] += house
-        request.session['activities'].insert(0, f'Earned {house} golds from the house!')
-    if earns == 'casino':
-        request.session['gold'] += casino
-        request.session['activities'].insert(0, f'Entered a casino and lost {casino} golds... Ouch...')
+        gold = random.randint(10, 20)
+        request.session['gold'] += gold
+    elif earns == 'cave':
+        gold = random.randint(5, 10)
+        request.session['gold'] += gold
+    elif earns == 'house':
+        gold = random.randint(2, 5)
+        request.session['gold'] += gold
+    elif earns == 'casino':
+        gold = random.randint(-50, 50)
+        if gold > 0:
+            request.session['gold'] += gold
+        elif gold < 0:
+            request.session['gold'] += gold
+            clase = 0
+        else:
+            request.session['gold'] += gold
+            clase = 2
+    message = f"Earned {gold} golds from the {earns}! ({date} {time})"
+    if gold < 0:
+        message = f"Entered a gold and lost {gold} golds... Ouch... ({date} {time})"
+    elif gold == 0:
+        message = f"Entered in a void, don't win and don't lose ({date} {time})"
+    activitiesobject = {
+        'clase': clase,
+        'message': message
+    }
+    request.session['activities'].insert(0, activitiesobject)
     return redirect('/')
 
 
